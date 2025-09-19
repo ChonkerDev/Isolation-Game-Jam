5d342f56dd9657b3fe9210271b893e6a131dde49
ChonkerDev <163789588+ChonkerDev@users.noreply.github.com>
2025-09-19 15:45:05 -0500

    added trigger action

---
+++ Assets/Chonker/Scripts/Common/TriggerAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Chonker.Scripts.Common {
+    public class TriggerAction : MonoBehaviour {
+        [SerializeField] private UnityEvent Event;
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            Event.Invoke();
+        }
+    }
+}
