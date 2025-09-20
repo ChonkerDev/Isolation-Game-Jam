f5c32b715740a343633b6e9471f074e0986a4562
ChonkerDev <163789588+ChonkerDev@users.noreply.github.com>
2025-09-19 23:44:34 -0500

    fix flowers

--- Assets/Chonker/Scripts/Level Parts/Collectable Flowers/CollectableFlower.cs
+++ Assets/Chonker/Scripts/Level Parts/Collectable Flowers/CollectableFlower.cs
@@ -9,7 +9,7 @@ public class CollectableFlower : MonoBehaviour {
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private UnityEvent _onCollected;
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.layer != LayerMask.GetMask("Player")) return;
+        if (other.gameObject.layer != LayerMask.NameToLayer("Player")) return;
         collectFlower();
     }
 
